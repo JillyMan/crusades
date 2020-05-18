75de4704e257b57a159d8d2a868e8f4fe3f4432d
JillyMan <artyom543211@gmail.com>
2020-05-18 23:10:59 +0300

    Clear main class.

--- src/main/java/com/crusades/App.java
+++ src/main/java/com/crusades/App.java
@@ -1,24 +1,13 @@
 package com.crusades;
-
 import com.crusades.core.input.KeyboardHandler;
 import com.crusades.core.window.GlfwWindow;
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
 
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
 
 public class App {
 
-     public static void main(String[] args) throws Exception {
-        try(GlfwWindow window = new GlfwWindow(1280, 720, "Our super duper game")) {
+     public static void main(String[] args) {
+        try (GlfwWindow window = new GlfwWindow(1280, 720, "Our super duper game")) {
             while(window.isOpen()) {
                 window.clear();
                 window.update();
@@ -41,7 +30,9 @@ public class App {
 
                 window.render();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
+     }
 
 }
